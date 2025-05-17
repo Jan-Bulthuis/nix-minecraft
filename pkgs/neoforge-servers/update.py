facd12175a4eaad30adecd3c651bac27d4ce3b64
Jan-Bulthuis <git@bulthuis.dev>
2025-05-17 16:21:59 +0200

    Updated lock file

--- pkgs/neoforge-servers/update.py
+++ pkgs/neoforge-servers/update.py
@@ -13,8 +13,6 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
 
 MAVEN = "https://maven.neoforged.net"
-NEO_MAVEN = f"{MAVEN}/releases"
-MOJANG_MAVEN = "https://libraries.minecraft.net"
 MANIFEST = "https://piston-meta.mojang.com/mc/game/version_manifest_v2.json"
 
 
@@ -26,83 +24,13 @@ def get_loader_versions():
     data = requests.get(
         f"{MAVEN}/api/maven/versions/releases/net/neoforged/neoforge"
     ).json()
-    versions = [
-        version for version in data["versions"] if version.startswith("21.1.172")
-    ]
+    versions = [version for version in data["versions"] if not "beta" in version]
     return versions
 
 
 def get_loader_game_version(loader_version):
-    return ".".join(["1"] + loader_version.split(".")[:2])
-
-
-def fetch_loader_version(loader_version):
-    """
-    Return the loader information for a given loader version
-    """
-    data = requests.get(
-        f"{MAVEN}/releases/net/neoforged/neoforge/{loader_version}/neoforge-{loader_version}-moddev-config.json"
-    ).json()
-    return data
-
-
-def get_library_data(library):
-    """
-    Return the URL for a given library.
-    """
-    maven = NEO_MAVEN
-
-    if library.startswith("com.mojang"):
-        maven = MOJANG_MAVEN
-
-    ret = {
-        "name": library,
-        "url": maven,
-    }
-    return ret
-
-
-def prefetch_libraries(logger, new_libraries, libraries):
-    logger = logger.getChild("libraries")
-    ret = []
-
-    print(new_libraries)
-
-    for library in new_libraries:
-        library = get_library_data(library)
-        name, url = library["name"], library["url"]
-
-        if not name in libraries or any(not v for k, v in libraries[name].items()):
-            logger.info(f"Fetching {name}")
-            nameparts = name.split(":")
-            ldir, lname, lversion = nameparts[:3]
-
-            lfilename = (
-                f"{lname}-{lversion}"
-                if len(nameparts) == 3
-                else f"{lname}-{lversion}-{nameparts[3]}"
-            )
-            lurl = "/".join(
-                (
-                    url.rstrip("/"),
-                    ldir.replace(".", "/"),
-                    lname,
-                    lversion,
-                    lfilename + ".jar",
-                )
-            )
-
-            lhash = subprocess.run(
-                ["nix-prefetch-url", lurl], capture_output=True, encoding="UTF-8"
-            ).stdout.rstrip("\n")
-
-            libraries[name] = {"name": lfilename + ".zip", "url": lurl, "sha256": lhash}
-        else:
-            logger.debug(f"Using cached {name}")
-
-        ret.append(name)
-
-    return ret
+    loader_parts = loader_version.split(".")
+    return ".".join(["1"] + loader_parts[: (2 if loader_parts[1] != "0" else 1)])
 
 
 minecraft_manifest = {}
@@ -131,13 +59,11 @@ def get_mapping_artifact(game_version):
     return mapping_artifacts[game_version]
 
 
-def gen_loader_locks(logger, version, versionData, libraries):
+def gen_loader_locks(logger, version):
     """
     Return the lock information for a given loader version
     """
-    installer_url = (
-        f"{NEO_MAVEN}/net/neoforged/neoforge/{version}/neoforge-{version}-installer.jar"
-    )
+    installer_url = f"{MAVEN}/releases/net/neoforged/neoforge/{version}/neoforge-{version}-installer.jar"
 
     ret = {
         "installer": {
@@ -145,15 +71,12 @@ def gen_loader_locks(logger, version, versionData, libraries):
             "sha256": requests.get(installer_url + ".sha256").text,
         },
         "mappings": get_mapping_artifact(get_loader_game_version(version)),
-        "mainClass": versionData["runs"]["server"]["main"],
-        "libraries": prefetch_libraries(logger, versionData["libraries"], libraries),
-        "full": versionData,
     }
 
     return ret
 
 
-def main(versions_loader, libraries, loader_locks, lib_locks):
+def main(versions_loader, loader_locks):
     """
     Fetch the relevant information and update the lockfiles.
     `versions` and `libraries` are data from the existing files, while
@@ -167,12 +90,10 @@ def main(versions_loader, libraries, loader_locks, lib_locks):
         loader_logger = logger.getChild("loader")
         for loader_version in loader_versions:
             if not versions_loader.get(loader_version, None):
-                loader_logger.info(f"Fetchinf version: {loader_version}")
+                loader_logger.info(f"Fetching version: {loader_version}")
                 versions_loader[loader_version] = gen_loader_locks(
                     loader_logger,
                     loader_version,
-                    fetch_loader_version(loader_version),
-                    libraries,
                 )
             else:
                 loader_logger.info(f"Version {loader_version} already locked")
@@ -180,9 +101,7 @@ def main(versions_loader, libraries, loader_locks, lib_locks):
         logger.warning("Cancelled fetching, writing and exiting")
 
     json.dump(versions_loader, loader_locks, indent=2)
-    json.dump(libraries, lib_locks, indent=2)
     loader_locks.write("\n")
-    lib_locks.write("\n")
 
 
 if __name__ == "__main__":
@@ -191,18 +110,9 @@ if __name__ == "__main__":
     lock_file.touch()
 
     build_support_folder = folder.parent / "build-support"
-    libraries_file = build_support_folder / "libraries.json"
-    libraries_file.touch()
 
-    with (
-        open(lock_file, "r") as loader_locks,
-        open(libraries_file, "r") as lib_locks,
-    ):
+    with (open(lock_file, "r") as loader_locks,):
         versions = {} if lock_file.stat().st_size == 0 else json.load(loader_locks)
-        libraries = {} if libraries_file.stat().st_size == 0 else json.load(lib_locks)
 
-    with (
-        open(lock_file, "w") as loader_locks,
-        open(libraries_file, "w") as lib_locks,
-    ):
-        main(versions, libraries, loader_locks, lib_locks)
+    with (open(lock_file, "w") as loader_locks,):
+        main(versions, loader_locks)
